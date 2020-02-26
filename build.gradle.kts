24e698e930797742623437893561d13e0de4379a
Daniel Schruhl <danielschruhl@gmail.com>
2020-02-26 11:05:57 +0100

    add mlflow client

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     application
     `java-library`
     id("com.github.maiflai.scalatest") version "0.26"
-    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 repositories {
@@ -15,12 +14,13 @@ val scalaCompatVersion = "2.12"
 
 dependencies {
     implementation("org.scala-lang:scala-library:$scalaVersion")
+    implementation("org.mlflow:mlflow-client:1.6.0")
+    implementation("com.github.scopt:scopt_$scalaCompatVersion:3.7.1")
+
     testImplementation("org.scalatest:scalatest_$scalaCompatVersion:3.0.8")
     testRuntimeOnly("org.pegdown:pegdown:1.4.2")
-    implementation("com.github.scopt:scopt_$scalaCompatVersion:3.7.1")
 }
 
 application {
-    executableDir = "lasagna"
     mainClassName = "lasagna.Main"
 }
